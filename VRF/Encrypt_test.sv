df6955d660f50c79b8fa49e3f30b639fc760d01b
Vasily Efimov <Protodwarf@gmail.com>
2015-03-24 14:15:36 +0300

    Тестовое окружение может делать файловый ввод/вывод.
    Теперь можно шифровать данные из файлов, записывать в файл, дешифровать.

--- VRF/Encrypt_test.sv
+++ VRF/Encrypt_test.sv
@@ -1,7 +1,6 @@
 program main_test();
 
-integer in_file, out_file;
-string line = "special snowflake super power";
+integer in_file, out_file, control_file;
 
 logic [0:79] key = 80'hd8ad98aa04d01b630bb4; //very random key
 logic [0:79] IV =  80'h3045eefcf5307b776266;  //very random IV
@@ -9,7 +8,7 @@ logic [0:92] s_1; //shifting part (key)
 logic [0:83] s_2; //shifting part (IV)
 logic [0:110] s_3;//shifting part (0's)
 
-logic [0:255] test_string;
+logic [0:7] middle_string;
 
 logic t_1, t_2, t_3; //temporary registers
 logic z; //cipher reg
@@ -65,42 +64,54 @@ end
 endfunction;
 
 initial begin
-/*
-	in_file = $fopen("cookie.txt","r");
+	trivium_id(); 
+//Opening files #######################################
+	in_file = $fopen("initial.txt","rb");
 	if(in_file == 0) begin
 		$display("Unable to open file.");
 	end
 	
-	out_file = $fopen("lol.txt");
+	out_file = $fopen("encrypted.txt","wb");
 	if(out_file == 0) begin
 		$display("Unable to open file.");
 	end
 	
-	while(!$feof(in_file)) begin
-		if($fgets(line,in_file)) begin
-			$fdisplay(out_file, line);
-		end
+	control_file = $fopen("result.txt","wb");
+	if(out_file == 0) begin
+		$display("Unable to open file.");
 	end
+//Opening files #######################################
+	while(!$feof(in_file))
+		begin
+		$fread(middle_string,in_file);
+		for(int i = 0; i < 8; i++)
+		begin
+			middle_string[i]=trivium_c(middle_string[i]);
+		end
+		$fwrite(out_file,"%s",middle_string);
+		end
 	
 	$fclose(in_file);
 	$fclose(out_file);
-	*/
-	
-	test_string = line;
-	$display("%s", test_string);
-	
 	trivium_id();
-	for (int i=0; i<256; i++)
-	begin
-		test_string[i]=trivium_c(test_string[i]);
-	end
-	$display("%s", test_string);
 	
-	trivium_id();
-	for (int i=0; i<256; i++)
-	begin
-		test_string[i]=trivium_c(test_string[i]);
+	out_file = $fopen("encrypted.txt","rb");
+	if(out_file == 0) begin
+		$display("Unable to open file.");
 	end
-	$display("%s", test_string);
+	
+	while(!$feof(out_file))
+	begin
+		$fread(middle_string,out_file);
+		for(int i = 0; i < 8; i++)
+		begin
+			middle_string[i]=trivium_c(middle_string[i]);
+		end
+		$fwrite(control_file,"%s",middle_string);
+		end
+	
+	$fclose(out_file);
+	$fclose(control_file);
+
 end
 endprogram
